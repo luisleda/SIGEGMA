5c28eda3fdb957a23fdb2aaf1012fbbf1dd179e3
luisleda <luis_neto@live.com>
2022-12-19 14:58:20 -0300

    commit front home

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="row border-top-e6 mt-4 ps-4 pt-4">
+    <p><em>Built with</em> <i class="bi bi-heart"></i>. License: <a href="https://github.com/changeweb/Unifiedtransform/blob/master/LICENSE" style="text-decoration: none;">GNU General Public License v3.0</a></p>
+    <p>Desenvolvido por equipe LABIGOV</p></br>
+    <p><em><b>Gestor do LABIGOV:</b> <a href="https://www.linkedin.com/in/oscar-adelino-costa-neto-00a27bb2/" style="text-decoration: none;">Oscar Adelino; </a></em><em><b>Desenvolvedor FullStack Pleno: </b> <a href="https://www.linkedin.com/in/luisleda/" style="text-decoration: none;">Luís Leda; </a></em><em><b>Estagiária Dev. Front-end: </b> <a href="https://www.linkedin.com/in/luisleda/" style="text-decoration: none;">Aline da Mata; </a></em> </p>
+</div>
