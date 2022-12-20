eb0c8930fd364acaa6dff98ad158ae9a0450a9ce
luisleda <luis_neto@live.com>
2022-12-20 16:25:14 -0300

    commit mod welcome blade

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,9 @@
 <div class="row border-top-e6 mt-4 ps-4 pt-4">
     <p><em>Built with</em> <i class="bi bi-heart"></i>. License: <a href="https://github.com/changeweb/Unifiedtransform/blob/master/LICENSE" style="text-decoration: none;">GNU General Public License v3.0</a></p>
     <p>Desenvolvido por equipe LABIGOV</p></br>
-    <p><em><b>Gestor do LABIGOV:</b> <a href="https://www.linkedin.com/in/oscar-adelino-costa-neto-00a27bb2/" style="text-decoration: none;">Oscar Adelino; </a></em><em><b>Desenvolvedor FullStack Pleno: </b> <a href="https://www.linkedin.com/in/luisleda/" style="text-decoration: none;">Luís Leda; </a></em><em><b>Estagiária Dev. Front-end: </b> <a href="https://www.linkedin.com/in/luisleda/" style="text-decoration: none;">Aline da Mata; </a></em> </p>
+    <p><em><b>Gestor do LABIGOV:</b> <a href="https://www.linkedin.com/in/oscar-adelino-costa-neto-00a27bb2/" style="text-decoration: none;">Oscar Adelino; </a></em><em><b>Desenvolvedor FullStack Pleno: </b> <a href="https://www.linkedin.com/in/luisleda/" style="text-decoration: none;">Luís Leda; </a></em><em><b>Estagiaria Dev. Front-end: </b> <a href="https://www.linkedin.com/in/aline-da-mata-7b93b318a/" style="text-decoration: none;">Aline da Mata; </a></em>
+    <em><b>Estagiario Dev. Front end:</b> <a href="https://www.linkedin.com/in/georgintin-filho-4a8304200/" style="text-decoration: none;">Georginton Filho; </a></em>
+    <em><b>Estagiario DBA:</b> <a href="https://www.linkedin.com/in/leanderson-silva-a104448b/" style="text-decoration: none;">Leanderson Silva; </a></em>
+    <em><b>Estagiaria Design :</b> <a href="https://www.linkedin.com/in/oscar-adelino-costa-neto-00a27bb2/" style="text-decoration: none;">Jucy Hellen Moraes; </a></em> 
+</p>
 </div>
--- resources/views/layouts/left-menu.blade.php
+++ resources/views/layouts/left-menu.blade.php
@@ -23,29 +23,29 @@
                                 }
                             }
                         @endphp
-                        <a class="nav-link d-flex {{ request()->is('classes')? 'active' : '' }}" href="{{url('classes')}}"><i class="bi bi-diagram-3"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Classes</span> <span class="ms-auto d-inline d-sm-none d-md-none d-xl-inline">{{ $classCount }}</span></a>
+                        <a class="nav-link d-flex {{ request()->is('classes')? 'active' : '' }}" href="{{url('classes')}}"><i class="bi bi-diagram-3"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Cursos</span> <span class="ms-auto d-inline d-sm-none d-md-none d-xl-inline">{{ $classCount }}</span></a>
                     </li>
                     @endcan
                     @if(Auth::user()->role != "student")
                     <li class="nav-item">
-                        <a type="button" href="#student-submenu" data-bs-toggle="collapse" class="d-flex nav-link {{ request()->is('students*')? 'active' : '' }}"><i class="bi bi-person-lines-fill"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Students</span>
+                        <a type="button" href="#student-submenu" data-bs-toggle="collapse" class="d-flex nav-link {{ request()->is('students*')? 'active' : '' }}"><i class="bi bi-person-lines-fill"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Discentes</span>
                             <i class="ms-auto d-inline d-sm-none d-md-none d-xl-inline bi bi-chevron-down"></i>
                         </a>
                         <ul class="nav collapse {{ request()->is('students*')? 'show' : 'hide' }} bg-white" id="student-submenu">
-                            <li class="nav-item w-100" {{ request()->routeIs('student.list.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('student.list.show')}}"><i class="bi bi-person-video2 me-2"></i> View Students</a></li>
+                            <li class="nav-item w-100" {{ request()->routeIs('student.list.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('student.list.show')}}"><i class="bi bi-person-video2 me-2"></i> Visualizar Discentes</a></li>
                             @if (!session()->has('browse_session_id') && Auth::user()->role == "admin")
-                            <li class="nav-item w-100" {{ request()->routeIs('student.create.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('student.create.show')}}"><i class="bi bi-person-plus me-2"></i> Add Student</a></li>
+                            <li class="nav-item w-100" {{ request()->routeIs('student.create.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('student.create.show')}}"><i class="bi bi-person-plus me-2"></i> Cadastrar Discente</a></li>
                             @endif
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a type="button" href="#teacher-submenu" data-bs-toggle="collapse" class="d-flex nav-link {{ request()->is('teachers*')? 'active' : '' }}"><i class="bi bi-person-lines-fill"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Teachers</span>
+                        <a type="button" href="#teacher-submenu" data-bs-toggle="collapse" class="d-flex nav-link {{ request()->is('teachers*')? 'active' : '' }}"><i class="bi bi-person-lines-fill"></i> <span class="ms-2 d-inline d-sm-none d-md-none d-xl-inline">Docentes</span>
                             <i class="ms-auto d-inline d-sm-none d-md-none d-xl-inline bi bi-chevron-down"></i>
                         </a>
                         <ul class="nav collapse {{ request()->is('teachers*')? 'show' : 'hide' }} bg-white" id="teacher-submenu">
-                            <li class="nav-item w-100" {{ request()->routeIs('teacher.list.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('teacher.list.show')}}"><i class="bi bi-person-video2 me-2"></i> View Teachers</a></li>
+                            <li class="nav-item w-100" {{ request()->routeIs('teacher.list.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('teacher.list.show')}}"><i class="bi bi-person-video2 me-2"></i> Visualizar Docentes</a></li>
                             @if (!session()->has('browse_session_id') && Auth::user()->role == "admin")
-                            <li class="nav-item w-100" {{ request()->routeIs('teacher.create.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('teacher.create.show')}}"><i class="bi bi-person-plus me-2"></i> Add Teacher</a></li>
+                            <li class="nav-item w-100" {{ request()->routeIs('teacher.create.show')? 'style="font-weight:bold;"' : '' }}><a class="nav-link" href="{{route('teacher.create.show')}}"><i class="bi bi-person-plus me-2"></i> Cadastrar Docentes</a></li>
                             @endif
                         </ul>
                     </li>
